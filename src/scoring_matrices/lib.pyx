9e1ede6ccb166775515f60e239ff4036a585a812
Martin Larralde <martin.larralde@embl.de>
2026-03-08 00:04:20 +0100

    Fix `linetrace` being enabled by default in Cython sources

--- src/scoring_matrices/lib.pyx
+++ src/scoring_matrices/lib.pyx
@@ -1,5 +1,5 @@
 # distutils: language = c
-# cython: language_level=3, linetrace=True, binding=True
+# cython: language_level=3, binding=True
 """Dependency free, Cython-compatible scoring matrices for bioinformatics.
 """
 
